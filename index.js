abce11ee10b937af7129f642cb8d0ebf5a2bdece
gmarvis <sgmarvis@gmail.com>
2023-08-19 19:41:43 +0100

    add css and some js

---
+++ index.js
@@ -0,0 +1,38 @@
+let firstInput = document.getElementById("firstInput");
+let secondInput = document.getElementById("secondInput");
+// let inputFill = document.getElementById('inputFill')
+let balance = document.querySelector("button");
+
+const myArray = Array(20)
+  .fill()
+  .map(() => Math.floor(20 * Math.random()));
+console.log(myArray);
+
+// let sum = myArray.reduce(function(a, b){
+//     return a + b
+// })
+
+balance.addEventListener("click", function () {
+  // const newArr = JSON.parse(inputFill.value);
+
+  for (let i = 0; i <= myArray.length; i++) {
+    for (let j = 0; j <= myArray.length; j++) {
+      if (+firstInput.value + myArray[i] === +secondInput.value + myArray[j]) {
+        document.querySelector(
+          "p"
+        ).innerHTML = `${myArray[i]} and ${myArray[j]}`;
+      }
+      // return;
+      else if (
+        +firstInput.value <= 0 ||
+        +firstInput.value > 20 ||
+        +secondInput.value <= 0 ||
+        +secondInput.value > 20
+      ) {
+        document.querySelector(
+          "p"
+        ).innerHTML = `numbers should be within the range of 1 and 20`;
+      }
+    }
+  }
+});
