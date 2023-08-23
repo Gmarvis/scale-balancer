5d27bf0cc232ec94d422d72a9042e4fcc688a037
gmarvis <sgmarvis@gmail.com>
2023-08-23 21:39:07 +0100

    updated the ui with error message, and another input field to take an array of numbers

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-const firstInput = document.getElementById('firstinput')
-const secondInput = document.getElementById('secondinput')
-// let inputFill = document.getElementById('inputFill')
-const balance = document.querySelector('button')
+let firstInput = document.getElementById("firstInput");
+let secondInput = document.getElementById("secondInput");
+let balance = document.querySelector("button");
+const selectArray = document.getElementById("myarray");
+const error = document.getElementById("error");
 
-const myArray = Array(20)
-  .fill()
-  .map(() => Math.floor(20 * Math.random()))
-console.log(myArray)
+balance.addEventListener("click", function () {
+  // get nunber from the input field an convert into an array
+  let number = +selectArray.value;
 
-// let sum = myArray.reduce(function(a, b){
-//     return a + b
-// })
-
-balance.addEventListener('click', function () {
+  let digits = number.toString().split("").map(Number);
+  const myArray = digits;
   // const newArr = JSON.parse(inputFill.value);
+
+  // return;
+  if (
+    +firstInput.value <= 0 ||
+    +firstInput.value > 20 ||
+    +secondInput.value <= 0 ||
+    +secondInput.value > 20
+  ) {
+    error.innerHTML = `<p>numbers should be within the range of 1 and 20</p>`;
+    return;
+  }
+
+  if (+selectArray.value <= 0) {
+    error.innerHTML = `<p>input weights to balance scale eg 1,2,3,4,5,6,7,8,9,0...</p>`;
+    return;
+  }
+  error.innerHTML = ``;
 
   for (let i = 0; i <= myArray.length; i++) {
     for (let j = 0; j <= myArray.length; j++) {
       if (+firstInput.value + myArray[i] === +secondInput.value + myArray[j]) {
         document.querySelector(
-          'p'
-        ).innerHTML = `${myArray[i]} and ${myArray[j]}`
-      } else if (
-        +firstInput.value <= 0 ||
-        +firstInput.value > 20 ||
-        +secondInput.value <= 0 ||
-        +secondInput.value > 20
-      ) {
-        document.querySelector('p').innerHTML =
-          'numbers should be within the range of 1 and 20'
+          "p"
+        ).innerHTML = `${myArray[i]} and ${myArray[j]}`;
+        return;
       }
     }
   }
-})
+  document.querySelector("p").innerHTML = ``;
+  error.innerHTML = `<p>Scale Imbalanced</p>`;
+});
