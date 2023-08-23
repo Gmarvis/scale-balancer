f18b62fa93451826cb21b019a737526b9b39bf49
gmarvis <sgmarvis@gmail.com>
2023-08-23 22:06:10 +0100

    js linter

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let firstInput = document.getElementById("firstInput");
-let secondInput = document.getElementById("secondInput");
-let balance = document.querySelector("button");
-const selectArray = document.getElementById("myarray");
-const error = document.getElementById("error");
+const firstInput = document.getElementById('firstInput')
+const secondInput = document.getElementById('secondInput')
+const balance = document.querySelector('button')
+const selectArray = document.getElementById('myarray')
+const error = document.getElementById('error')
 
-balance.addEventListener("click", function () {
+balance.addEventListener('click', function () {
   // get nunber from the input field an convert into an array
-  let number = +selectArray.value;
+  const number = +selectArray.value
 
-  let digits = number.toString().split("").map(Number);
-  const myArray = digits;
+  const digits = number.toString().split('').map(Number)
+  const myArray = digits
   // const newArr = JSON.parse(inputFill.value);
 
   // return;
@@ -19,26 +19,26 @@ balance.addEventListener("click", function () {
     +secondInput.value <= 0 ||
     +secondInput.value > 20
   ) {
-    error.innerHTML = `<p>numbers should be within the range of 1 and 20</p>`;
-    return;
+    error.innerHTML = '<p>numbers should be within the range of 1 and 20</p>'
+    return
   }
 
   if (+selectArray.value <= 0) {
-    error.innerHTML = `<p>input weights to balance scale eg 1,2,3,4,5,6,7,8,9,0...</p>`;
-    return;
+    error.innerHTML = '<p>input weights to balance scale eg 1,2,3,4,5,6,7,8,9,0...</p>'
+    return
   }
-  error.innerHTML = ``;
+  error.innerHTML = ''
 
   for (let i = 0; i <= myArray.length; i++) {
     for (let j = 0; j <= myArray.length; j++) {
       if (+firstInput.value + myArray[i] === +secondInput.value + myArray[j]) {
         document.querySelector(
-          "p"
-        ).innerHTML = `${myArray[i]} and ${myArray[j]}`;
-        return;
+          'p'
+        ).innerHTML = `${myArray[i]} and ${myArray[j]}`
+        return
       }
     }
   }
-  document.querySelector("p").innerHTML = ``;
-  error.innerHTML = `<p>Scale Imbalanced</p>`;
-});
+  document.querySelector('p').innerHTML = ''
+  error.innerHTML = '<p>Scale Imbalanced</p>'
+})
